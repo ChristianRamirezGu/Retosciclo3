3881d5770ce1ea016d52dabe7cc31618b62d7075
JennyYaya <jenny.yaya.mt@usa.edu.co>
2022-10-15 19:51:32 -0500

    retosciclo3

---
+++ src/main/java/com/entrega/reto3/Controller/ReservationController.java
@@ -0,0 +1,72 @@
+package com.entrega.reto3.Controller;
+
+import com.entrega.reto3.CrudRepository.ReservationCrudRepository;
+import com.entrega.reto3.Model.Reservation;
+import com.entrega.reto3.Repository.CountClient;
+import com.entrega.reto3.Service.ReservationService;
+import com.entrega.reto3.Service.Status;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "*", methods ={RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE })
+@RestController
+@RequestMapping("/api/Reservation")
+public class ReservationController {
+
+
+    @Autowired
+    private ReservationService reservationService;
+
+    @GetMapping("/all")
+    public List<Reservation> getAll() {
+        return reservationService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Reservation> getReservation(@PathVariable("id") int id) {
+        return reservationService.getReservation(id);
+    }
+
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Reservation save(@RequestBody Reservation reservation) {
+        return reservationService.save(reservation);
+    }
+
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Reservation update(@RequestBody Reservation reservation){
+        return reservationService.update(reservation);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public boolean delete(@PathVariable("id")int reservationId) {
+        return reservationService.delete(reservationId);
+
+    }
+
+///////////////reto5
+
+    @GetMapping("/report-clients")
+    public List<CountClient> getReservationsReportClient(){
+        return reservationService.getTopClients();
+    }
+
+    @GetMapping("/report-dates/{dateOne}/{dateTwo}")
+    public List<Reservation> getReservationsReportDates(@PathVariable("dateOne") String dateOne,@PathVariable("dateTwo") String dateTwo){
+        return reservationService.informePeriodoTiempoReservas(dateOne,dateTwo);
+    }
+
+    @GetMapping("/report-status")
+    public Status getReservationsStatusReport(){
+        return reservationService.getReservationStatusReport();
+    }
+
+}
